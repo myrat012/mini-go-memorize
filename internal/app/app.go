6579a4715255f06f0f49bdb404d7448ef25dc28c
agamyrat.e <agamyrat.e@millione.tm>
2024-02-06 17:27:26 +0500

    end with frontend

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	"image/color"
 
 	"fyne.io/fyne/v2"
@@ -8,6 +9,7 @@ import (
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -52,19 +54,74 @@ func (m *Memorize) Start() {
 	)
 	mainWindow.SetContent(mainContainer)
 
-	mMenu := mainMenu(mainWindow)
+	mMenu := mainMenu(mainWindow, mainContainer, m)
 	mainWindow.SetMainMenu(fyne.NewMainMenu(mMenu))
 
 	mainWindow.ShowAndRun()
 }
 
-func mainMenu(mainWindow fyne.Window) *fyne.Menu {
-	item1 := fyne.NewMenuItem("Settings", func() {})
-	item2 := fyne.NewMenuItem("Quit", func() {
+func mainMenu(mainWindow fyne.Window, mainContainer *fyne.Container, m *Memorize) *fyne.Menu {
+
+	// settings container
+	questionCount := widget.NewEntry()
+	questionCount.SetPlaceHolder("How much question you want?")
+	radio := widget.NewRadioGroup([]string{"Dark Theme", "Light Theme"}, func(value string) {
+		if value == "Dark Theme" {
+			m.App.Settings().SetTheme(theme.DarkTheme())
+		} else {
+			m.App.Settings().SetTheme(theme.LightTheme())
+		}
+	})
+
+	settingsContainer := container.New(
+		layout.NewVBoxLayout(),
+		widget.NewCheck("Want Randomly?", func(b bool) {
+			if b {
+				fmt.Println("Checked")
+			} else {
+				fmt.Println("Uncheked")
+			}
+		}),
+		questionCount,
+		radio,
+		widget.NewButton("Save", func() {
+
+		}),
+	)
+
+	// add word container
+	originalWord := widget.NewEntry()
+	originalWord.SetPlaceHolder("Word")
+
+	translatedWord := widget.NewEntry()
+	translatedWord.SetPlaceHolder("Translate")
+
+	addWordContainer := container.New(
+		layout.NewVBoxLayout(),
+		originalWord,
+		translatedWord,
+		widget.NewButton("Save", func() {
+			originalWord.SetText("")
+			translatedWord.SetText("")
+		}),
+	)
+
+	item1 := fyne.NewMenuItem("Settings", func() {
+		mainWindow.SetContent(settingsContainer)
+	})
+	item2 := fyne.NewMenuItem("Back", func() {
+		mainWindow.SetContent(mainContainer)
+	})
+
+	item3 := fyne.NewMenuItem("Add-Words", func() {
+		mainWindow.SetContent(addWordContainer)
+	})
+
+	item4 := fyne.NewMenuItem("Quit", func() {
 		mainWindow.Close()
 	})
 	return &fyne.Menu{
 		Label: "File",
-		Items: []*fyne.MenuItem{item1, item2},
+		Items: []*fyne.MenuItem{item1, item2, item3, item4},
 	}
 }
